c93066e9d9eeb5ea1ab54fc21eea1d9afceb6d6a
212578670 <hilal1.tarakci@ge.com>
2018-06-22 16:47:41 +0300

    [add] tutor part 1

--- 01-basic-routing/src/components/App.js
+++ 01-basic-routing/src/components/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+
+import Home from './HomeComponent';
+import Hello from './HelloComponent';
+import About from './AboutComponent';
+import Books from './BooksComponent';
 
 // import route Components here
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <ul>
-            <li><a href="">Hello</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Books</a></li>
-          </ul>
-          <hr/>
-          {/* Routes will go here */}
-      </div>
-  </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <ul>
+              <li><Link to="/hello">Hello</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/books">Books</Link></li>
+            </ul>
+            <hr/>
+            {/* Routes will go here */}
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/hello" component={Hello} />
+              <Route path="/books" component={Books} />
+          </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
